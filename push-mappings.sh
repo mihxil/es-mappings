ab0ebce59058a0402724b132de2ce56fde1f3352
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-10-08 11:25:23 +0200

    generalized

--- push-mappings.sh
+++ push-mappings.sh
@@ -4,97 +4,130 @@ if [ "$DEBUG" = "true" ] ; then
     set -x
 fi
 
-if [ $# -lt 1 ];
-then
-    echo "Usage $0 <es-url> [<index number>|<alias>]"
-    echo "index number:  Number of the new index to create (e.g. 2 in apimedia-2). If omitted the mappings are put over the old ones (only possible if they are compatible)"
-    echo "Each index has 2 aliases: apimedia (read) apimedia-publish (write)."
-    echo "This script will create a new index and point the publisher to it."
-    echo "You will have to manually move the apimedia after you copied the index"
-    exit
-fi
+commandline_args=("$@")
 
-desthost=$1
 
-basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-echo $basedir
-needssettings=false
-if [ "$2" == "" ] ; then
-    echo "No index number found, trying to put mappings over existing ones (supposing they are compatible)"
-    destindex=apimedia
-else
-    if [[ $2 =~ ^[0-9]+$ ]] ; then
-        if (( $2 > 0 )) ; then
-            previndex=apimedia-$(($2-1))
-        fi
-        destindex=apimedia-$2
-        needssettings=true
-    else
-        echo "No index number found, trying to put mappings over existing ones (supposing they are compatible)"
-        destindex=$2
+function _init() {
+    basename=$1
+    if [ ${#commandline_args[@]} -lt 1 ];
+    then
+        echo "Usage $0 <es-url> [<index number>|<alias>]"
+        echo "index number:  Number of the new index to create (e.g. 2 in $basename-2). If omitted the mappings are put over the old ones (only possible if they are compatible)"
+        echo "Each index has 2 aliases: $basename (read) $basenamea-publish (write)."
+        echo "This script will create a new index and point the publisher to it."
+        echo "You will have to manually move the apimedia after you copied the index"
+        exit
     fi
-    echo "$previndex -> $destindex"
+    desthost=${commandline_args[0]}
+    number=${commandline_args[1]}
 
-fi
-
-echo "Echo putting $basedir to $desthost/$destindex"
-
-declare -a arr=( "group" "program" "segment" "deletedprogram" "deletedgroup" "deletedsegment" "cue" "programMemberRef" "groupMemberRef" "segmentMemberRef" "episodeRef" )
-rm $destindex.json
-echo '{' > $destindex.json
-if [ "$needssettings" = true ]; then
-    echo "putting settings"
-    echo '"settings":' >> $destindex.json
-    cat $basedir/setting/apimedia.json >> $destindex.json
-    echo "," >> $destindex.json
-
-    echo '"mappings": {' >> $destindex.json
-
-
-    for i in "${!arr[@]}"
-    do
-        mapping=${arr[$i]}
-        if [ $i -gt 0 ]; then
-            echo "," >> $destindex.json
+    echo "basename $basename"
+    needssettings=false
+    if [ "$number" == "" ] ; then
+        echo "No index number found, trying to put mappings over existing ones (supposing they are compatible)"
+        destindex=$basename
+    else
+        if [[ $number =~ ^[0-9]+$ ]] ; then
+            if (( $number > 0 )) ; then
+                seq=$(($number-1))
+                previndex="$basename-$seq"
+            fi
+            destindex="$basename-$number"
+            needssettings=true
+        else
+            echo "No index number found, trying to put mappings over existing ones (supposing they are compatible)"
+            destindex=$number
         fi
-        echo '"'$mapping'": ' >>  $destindex.json
-        cat $basedir/mapping/$mapping.json >> $destindex.json
-    done
-    echo -e '}\n}' >> $destindex.json
+        echo "$previndex -> $destindex"
 
-    echo Created $destindex.json
-    curl -XPUT -H'content-type: application/json' $desthost/$destindex -d@$destindex.json
-else
-    echo "previndex $previndex . No settings necessary"
-    for i in "${!arr[@]}"
-    do
-        mapping=${arr[$i]}
-        echo curl -XPUT -H'content-type: application/json' $desthost/$destindex/$mapping/_mapping -d@$basedir/mapping/$mapping.json
-        curl -XPUT -H'content-type: application/json' $desthost/$destindex/$mapping/_mapping -d@$basedir/mapping/$mapping.json
-    done
-fi
+    fi
 
 
-if [ "$previndex" != "" ] ; then
 
-   echo "moving alias $previndex $destindex"
+}
 
-   publishalias="{
-        \"actions\": [
-            { \"remove\": {
-                \"alias\": \"apimedia-publish\",
-                \"index\": \"$previndex\"
-            }},
-            { \"add\": {
-                \"alias\": \"apimedia-publish\",
-                \"index\": \"$destindex\"
-            }}
-        ]
-      }"
-   echo $publishalias
-   echo
-   curl -XPOST $desthost/_aliases -d "$publishalias"
-   echo
+function put() {
+    basedir=$1
+    shift
+    _init $1
+    shift
+    echo "Echo putting $basedir to $desthost/$destindex"
+    local arr=("$@")
+    rm $destindex.json
+    echo '{' > $destindex.json
+    if [ "$needssettings" = true ]; then
+        echo "putting settings"
+        echo '"settings":' >> $destindex.json
+        settingfile=$basedir/setting/$basename.json
+        if [ ! -e $settingfile ] ; then
+           echo "Setting file $settingfile doesn't exist"
+           exit
+        fi
+        cat $basedir/setting/$basename.json >> $destindex.json
+        echo "," >> $destindex.json
+        echo '"mappings": {' >> $destindex.json
+
+        for i in "${!arr[@]}"
+        do
+            mapping=${arr[$i]}
+            echo $mapping
+            if [ $i -gt 0 ]; then
+                echo "," >> $destindex.json
+            fi
+            echo '"'$mapping'": ' >>  $destindex.json
+            cat $basedir/mapping/$mapping.json >> $destindex.json
+        done
+        echo -e '}\n}' >> $destindex.json
+
+        echo Created $destindex.json
+        curl -XPUT -H'content-type: application/json' $desthost/$destindex -d@$destindex.json
+    else
+        echo "previndex $previndex . No settings necessary"
+        for i in "${!arr[@]}"
+        do
+            mapping=${arr[$i]}
+            echo curl -XPUT -H'content-type: application/json' $desthost/$destindex/$mapping/_mapping -d@$basedir/mapping/$mapping.json
+            curl -XPUT -H'content-type: application/json' $desthost/$destindex/$mapping/_mapping -d@$basedir/mapping/$mapping.json
+        done
+    fi
+
+    ## Now aliases
+    publishalias='{"actions": ['
+    if [ $number -gt 0 ] ; then
+        # updating an index. Remove the exstign publish alias
+        # remove publish alias
+        publishalias="$publishalias
+        {
+         \"remove\": {
+           \"alias': \"$basename-publish\",
+           \"index\": \"$previndex\"
+         }},"
+    else
+        # completely new index. Create also an api alias
+        publishalias="$publishalias
+        {
+
+            \"add\": {
+              \"alias\": \"$basename\",
+              \"index\": \"$destindex\"
+         }},"
+    fi
+    # Create a publish alias for the new index in any case
+    publishalias="$publishalias
+  {
+
+            \"add\": {
+              \"alias\": \"$basename-publish\",
+              \"index\": \"$destindex\"
+         }}]}"
+    echo $publishalias
+    curl -XPOST -H'content-type: application/json'  $desthost/_aliases -d "$publishalias"
+
+    reindex="{
+        \"source\": { \"index\": \"$previndex\" },
+        \"dest\": { \"index\": \"$destindex\" }
+     }"
+    echo curl -XPOST $desthost/_reindex -d "\"$reindex\""
 
 # Copy index
   echo
@@ -133,3 +166,7 @@ if [ "$previndex" != "" ] ; then
 #End move apimedia
 
 fi
+
+
+
+}
